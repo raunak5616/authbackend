45aebbe032fc458c3de8c030cdf4db3696abd6b8
Raunak Kumar <raunakkh8789@gmail.com>
2026-05-08 13:00:16 +0530

    feat: update project structure and add server setup
    - Changed main entry point from index.js to server.js
    - Added development script for nodemon
    - Updated package.json to include necessary dependencies and devDependencies
    - Created server.js to set up a basic Express server with CORS and JSON parsing
    - Implemented a simple GET route to confirm server is running

---
+++ server.js
@@ -0,0 +1,14 @@
+import express from 'express';
+import cors from 'cors';
+
+const app = express();
+app.use(express.json());
+app.use(cors());
+
+app.get('/',(req,res)=>{
+    res.send('Server is running🚀');
+})
+
+app.listen(5000,()=>{
+    console.log('Server is running on port 5000');
+});
